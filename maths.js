a684da6c30495260ec771599d3de1f7ac8defcbe
avinashmth898 <47936739+avinashmth898@users.noreply.github.com>
2023-12-08 14:03:47 +0000

    maths

---
+++ maths.js
@@ -0,0 +1,17 @@
+const mobile= new Number(9521610418)
+console.log(Math)
+console.log(Math.abs(-8768))
+console.log(Math.ceil(4.8))
+console.log(Math.floor(8.7))
+console.log(Math.round(5.4))
+
+console.log(Math.sqrt(49))
+console.log(Math.max(89,54,67,786))
+console.log(Math.min(89,54,67,786))
+
+console.log((Math.random()*10)+1)
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max-min +1) + min))
---
+++ numbers.js
@@ -0,0 +1,11 @@
+const mobile= new Number(9521610418)
+//when you print an object it always comes with the varible type
+console.log(mobile)
+console.log(mobile.toFixed(2))
+
+console.log(typeof(mobile.toString()))
+const decimal=72.2698
+console.log(decimal.toPrecision(3))
+const decimal_other=641.354561
+console.log(decimal_other.toPrecision(3))
+console.log(mobile.toLocaleString('en-IN'))
