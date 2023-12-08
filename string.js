a58dbdf18615055858c2199f429716535c11f17f
avinashmth898 <47936739+avinashmth898@users.noreply.github.com>
2023-12-08 05:15:43 +0000

    Pending changes exported from your codespace

---
+++ string.js
@@ -0,0 +1,11 @@
+const name= "Avinsah"
+const Age=25
+const mob_no=9521610418
+
+console.log(`Hello there my name is ${name} and my age is ${Age} and you can contact me @ ${mob_no}`);
+const gameName= new String("Avinash")
+console.log(gameName.__proto__);
+console.log(gameName.length)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
