95f536acbfa1f559a5360f5ef89a9b8b67620a3b
avinashmth898 <47936739+avinashmth898@users.noreply.github.com>
2023-12-12 20:00:50 +0000

    Conditional control flow and truthy

---
+++ ControlFlow/switch.js
@@ -0,0 +1,43 @@
+const month=3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("none matched")
+        break;
+}
+
+const months="feb"
+
+switch (months) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("none matched")
+        break;
+}
